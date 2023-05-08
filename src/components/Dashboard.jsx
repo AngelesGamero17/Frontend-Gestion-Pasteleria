ffc28b659de0feff7cadfbc3565b00b8830ef608
AngelesGamero17 <angelesgamero17outoolk.com>
2023-05-07 19:52:42 -0500

    first commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Header from "../template/Header";
+import { Apiurl } from "../services/apirest";
+import axios from "axios";
+
+class Dashboard extends React.Component {
+  state = {
+    empleados: [],
+  };
+
+  clickEmpleado(id) {
+    window.location.href = "./editar/" + id;
+  }
+
+  componentDidMount() {
+    let url = Apiurl + "empleado";
+    axios.get(url).then((response) => {
+      this.setState({
+        empleados: response.data,
+      });
+    });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <Header></Header>
+        <div className="container">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">NOMBRE</th>
+                <th scope="col">APELLIDO</th>
+                <th scope="col">EMAIL</th>
+                <th scope="col">FECHA DE NACIMENTO</th>
+                <th scope="col">DIRECCION</th>
+                <th scope="col">TELEFONO</th>
+                <th scope="col">INGRESO</th>
+                <th scope="col">TIPO DE EMPLEADO</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.empleados.map((value, index) => {
+                return (
+                  <tr key={index} onClick={() => this.clickEmpleado(value.ID)}>
+                    <th scope="row">{value.ID}</th>
+                    <td>{value.nomEmp}</td>
+                    <td>{value.apellEmp}</td>
+                    <td>{value.email}</td>
+                    <td>{value.fecNac}</td>
+                    <td>{value.direccEmp}</td>
+                    <td>{value.telefono}</td>
+                    <td>{value.ingreso}</td>
+                    <td>{value.tipoEmpleado}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/Editar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Apiurl } from '../services/apirest';
+
+class Editar extends React.Component{
+
+
+    render(){
+        return(
+            <div>
+                hola desde el Editar
+            </div>
+
+        );
+    }
+
+}
+
+export default Editar
